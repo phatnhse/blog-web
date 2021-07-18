33f8e4fa72406a44ff0749bea5f6ebfc36379511
phatnhse <11498224+phatnhse@users.noreply.github.com>
2021-07-18 14:59:04 +0700

    Migrate to mdx bundle (#6)
    Co-authored-by: phatnguyen <phat.nguyenhoang@lazada.com>

--- components/BlogPost.js
+++
@@ -1,9 +0,0 @@
-export default function BlogPost({ children, meta }) {
-  return (
-    <>
-      <div className="base-container prose prose-yellow">
-        <article>{children}</article>
-      </div>
-    </>
-  )
-}
