30096de79a1715a0f11d82c74909bd116f93ccf5
phatnhse <11498224+phatnhse@users.noreply.github.com>
2021-07-20 10:50:51 +0700

    Update UI post detail page (#13)
    Co-authored-by: phatnguyen <phat.nguyenhoang@lazada.com>

--- components/Post.js
+++ components/Post.js
@@ -4,7 +4,7 @@ export const Post = ({ post }) => {
   return (
     <>
       <Link href={`posts/${post.slug}`}>
-        <div className="pt-4 pb-4 mt-4 cursor-pointer max-w-md">
+        <div className="pt-4 pb-2 cursor-pointer max-w-md">
           <h1 className="font-bold text-xl text-black">{post.frontmatter.title}</h1>
           <div>
             <p className="font-light text-base text-black">{`${post.frontmatter.description.substring(0, 150)} ...`}</p>
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,13 +1,24 @@
-import React, {useMemo} from 'react'
-import {getMDXComponent} from 'mdx-bundler/client'
+import React, { useMemo } from 'react'
+import { getMDXComponent } from 'mdx-bundler/client'
 import { getAllPosts, getSinglePost } from "../../utils/PostRepo"
 
 const Post = ({ code, frontmatter }) => {
+  const tags = frontmatter.tags ? frontmatter.tags.split(',') : []
   const Component = useMemo(() => getMDXComponent(code), [code]);
+
   return (
-    <div className="base-container prose prose-lg max-w-none">
-      <h1>{frontmatter.title}</h1>
-      <Component />
+    <div className="base-container prose prose-lg max-w-none mt-24 mb-24">
+      <h1 className="text-center">{frontmatter.title}</h1>
+      <p className="text-center text-gray-500 text-base">{frontmatter.description}</p>
+      <div className="flex justify-center">
+        {tags.map(tag => {
+          return <span id={tag} className="rounded pt-1 pb-1 pl-2 pr-2 text-base bg-gray-200 mr-2 ml-2">{tag}</span>
+        })}
+      </div>
+
+      <div className="mt-16 mb-16">
+        <Component />
+      </div>
     </div>
   );
 };
