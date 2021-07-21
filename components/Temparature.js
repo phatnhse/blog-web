73edcb125629637059d027d28e878d2582c25836
phatnguyen <phat.nguyenhoang@lazada.com>
2021-07-21 21:50:20 +0700

    Resolve build issue

--- components/Temparature.js
+++ components/Temparature.js
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { useEffect } from "react/cjs/react.development"
+import { useEffect, useState } from "react"
 import Link from "next/link";
 import Api from "../utils/WeatherAPI"
 
