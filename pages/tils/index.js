33631ed39fb0740d6ee0a70cf2907068c071c860
phatnhse <11498224+phatnhse@users.noreply.github.com>
2021-07-26 10:19:53 +0700

    Add til (#21)
    Co-authored-by: phatnguyen <phat.nguyenhoang@lazada.com>

--- pages/tils/index.js
+++ pages/tils/index.js
@@ -1,17 +1,29 @@
-export default function Tils({ talks }) {
+import { getAllTils } from '../../utils/TilRepo';
+import Til from '../../components/Til';
+
+
+export default function Tils({ tils }) {
   return (
     <>
       <div className="base-container pt-6 pt-6">
-        <h1 className="font-medium text-sm text-gray-500 mt-12">TODAY I LEARN</h1>
+        <h1 className="font-medium text-sm text-gray-500 mt-20">TODAY I LEARN</h1>
+        <ul>
+          {tils.map((til, index) => (
+            <li key={`tit-${index}`}>
+              <Til til={til} />
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
 }
 
 export const getStaticProps = async () => {
-  const talks = []
+  const tils = getAllTils();
+  console.log(tils)
 
   return {
-    props: { talks },
+    props: { tils },
   };
 };
---
+++ utils/TilRepo.js
@@ -0,0 +1,39 @@
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+import { bundleMDX } from "mdx-bundler"
+
+export const PATH = path.join(process.cwd(), "/data/tils")
+
+export function getSourceOfFile(fileName) {
+  return fs.readFileSync(path.join(PATH, fileName))
+}
+
+export function getAllTils() {
+  return fs
+    .readdirSync(PATH)
+    .filter((path) => /\.mdx?$/.test(path))
+    .map((fileName) => {
+      const source = getSourceOfFile(fileName)
+      const slug = fileName.replace(/\.mdx?$/, "")
+      const { data } = matter(source)
+
+      return {
+        frontmatter: data,
+        slug: slug,
+      }
+    })
+}
+
+export async function getSingleTil(slug) {
+  const source = getSourceOfFile(slug + ".mdx")
+
+  const { code, frontmatter } = await bundleMDX(source, {
+    cwd: PATH,
+  })
+
+  return {
+    frontmatter,
+    code,
+  }
+}
