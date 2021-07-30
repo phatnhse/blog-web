5c9846041c90c5284e81c7f22c277a5cbda93a73
phatnhse <11498224+phatnhse@users.noreply.github.com>
2021-07-30 17:52:36 +0700

    How I come up with personal blog mdx (#26)
    Co-authored-by: phatnguyen <phat.nguyenhoang@lazada.com>

--- data/posts/tokyo/LittleTokyo.jsx
+++ data/posts/tokyo/LittleTokyo.jsx
@@ -1,17 +1,22 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import Model from "./Model";
 
 export default function LittleTokyo() {
   return (
     <>
-      <div className="w-full h-96">
-        <Canvas camera={{ position: [0, 15, 500] }}>
-          <Suspense fallback={null}>
-            <Model/>
-            <OrbitControls />
-            <Environment preset="sunset" background={false} />
+      <div className="w-full" style={{
+        height: '600px',
+        background: '#bfe3dd'
+      }}>
+        <Canvas camera={{ position: [1, 0, 750] }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={2} />
+          <directionalLight position={[-10, -10, -5]} intensity={1} />
+          <OrbitControls autoRotate={true} />
+          <Suspense fallback={'loading'}>
+            <Model />
           </Suspense>
         </Canvas>
       </div>
