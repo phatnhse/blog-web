c4e22eb8acff370e8b472c380e72b3b2c4600bfc
phatnguyen <phat.nguyenhoang@lazada.com>
2021-08-02 09:31:07 +0700

    Add zoom function for littel tokyo

--- data/posts/tokyo/LittleTokyo.jsx
+++ data/posts/tokyo/LittleTokyo.jsx
@@ -2,12 +2,19 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Html, useProgress } from "@react-three/drei";
 import Model from "./Model";
+import { useState } from 'react';
 
 export default function LittleTokyo() {
+
+  const [enableZoom, setEnableZoom] = useState(false)
 
   function Loader() {
     const { active, progress, errors, item, loaded, total } = useProgress();
     return <Html center> <div className="text-medium"> loading {progress} % </div></Html>;
+  }
+
+  const onClick = () => {
+    setEnableZoom(!enableZoom)
   }
 
   return (
@@ -22,11 +29,15 @@ export default function LittleTokyo() {
             <ambientLight intensity={1} />
             <directionalLight intensity={1} />
             <directionalLight intensity={1} />
-            <OrbitControls enableZoom={false} autoRotate={true} />
-            <Model scale={0.008}/>
+            <OrbitControls enableZoom={enableZoom} autoRotate={true} />
+            <Model scale={0.008} />
           </Suspense>
         </Canvas>
       </div>
+
+      <a className="flex mt-2 justify-center text-base text-gray-600 !font-normal !no-underline" onClick={onClick}>
+        click here to {enableZoom ? 'disable' : 'enable'} zoom
+      </a>
     </>
   )
 }
