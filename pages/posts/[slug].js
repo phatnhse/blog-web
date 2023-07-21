7c4859006163d88e39d459f95277237f4cbece96
phatnhse <phatnhse@gmail.com>
2023-07-21 13:50:16 +0700

    Add new post about declarative in jetpack compose

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,47 +1,46 @@
-import React, { useMemo } from 'react'
-import { getMDXComponent } from 'mdx-bundler/client'
-import { getAllPosts, getSinglePost } from "../../utils/PostRepo"
+import React, {useMemo} from 'react'
+import {getMDXComponent} from 'mdx-bundler/client'
+import {getAllPosts, getSinglePost} from "../../utils/PostRepo"
 import InlineCode from '../../components/markdown/InlineCode';
 import BlockQuote from '../../components/markdown/BlockQuote';
 import PreCode from '../../components/markdown/PreCode';
 import CustomLink from '../../components/markdown/CustomLink';
 
-const Post = ({ code, frontmatter }) => {
+const Post = ({code, frontmatter}) => {
   const Component = useMemo(() => getMDXComponent(code), [code]);
 
   return (
-    <div className="container-medium max-w-none mt-24 mb-0">
+    <div className="container-medium  mt-24 mb-0">
       <p className="text-center text-4xl font-bold">{frontmatter.title}</p>
       <div className="flex justify-center font-medium text-sm text-gray-600 mt-4 mb-4">
         {frontmatter.publishedOn}
       </div>
+
       <div className="flex justify-center">
-
-      </div>
-
-      <div className="mt-16 mb-16 prose">
-        <Component components={
-          {
-            'code': InlineCode,
-            'blockquote': BlockQuote,
-            'a': CustomLink,
-            'pre': PreCode
-          }
-        } />
+        <div className="mt-16 mb-16 prose">
+          <Component components={
+            {
+              'code': InlineCode,
+              'blockquote': BlockQuote,
+              'a': CustomLink,
+              'pre': PreCode
+            }
+          }/>
+        </div>
       </div>
     </div>
   );
 };
 
-export const getStaticProps = async ({ params }) => {
+export const getStaticProps = async ({params}) => {
   const post = await getSinglePost(params.slug);
   return {
-    props: { ...post },
+    props: {...post},
   };
 };
 
 export const getStaticPaths = async () => {
-  const paths = getAllPosts().map(({ slug }) => ({ params: { slug } }));
+  const paths = getAllPosts().map(({slug}) => ({params: {slug}}));
   return {
     paths,
     fallback: false,
